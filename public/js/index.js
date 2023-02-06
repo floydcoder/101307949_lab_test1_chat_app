91b03c283af4bc4b37625881438328b8939282f7
Marco Stevanella <marcostevanella.canada@gmail.com>
2023-02-06 14:14:46 -0500

    add login signup redirections

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+// Redirect User to LogIn
+function redirectToLogin() {
+  window.location.href = 'login.html';
+}
+
+// Redirect User to SignUp
+function redirectToSignup() {
+  window.location.href = 'index.html';
+}
--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const http = require('http');
+const path = require('path');
+const socketio = require('socket.io');
 const mongoose = require('mongoose');
-const connectDB = require('./database/config');
+const connectDB = require('./server/database/config');
 const PORT = process.env.PORT || 3500;
 
 // Connect to MongoDB
 connectDB();
+
+// Create Server
+const server = http.createServer(app);
+
+// Serve Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Create Instance of socketIo
+const io = socketio(server);
 
 mongoose.connection.once('open', () => {
   console.log('Connected to MongoDB');
