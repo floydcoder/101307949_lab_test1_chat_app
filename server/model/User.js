5f0bc0d75dad9076bf49c3808ac08218e0bd6e14
Marco Stevanella <marcostevanella.canada@gmail.com>
2023-02-06 02:02:59 -0500

    add User mongoose model

--- server/model/User.js
+++ server/model/User.js
@@ -1,2 +1,17 @@
+const mongoose = require('mongoose');
 
-
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Username is required'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  createdon: {
+    type: Date,
+    default: Date.now,
+  },
+});
